d1b05da6a511593195950ebe993ed2fc46f7f197
KonstantinosKoulaxizis <kkoulaxizis07@gmail.com>
2022-09-26 10:17:51 +0200

    feat: Game view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Outlet } from 'react-router-dom'
 
 import GameSelection from './views/GameSelection'
+import Game from './views/Game'
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<GameSelection />} />
+        <Route path='/game' element={<Game />} />
       </Route>
     </Routes>
   )
