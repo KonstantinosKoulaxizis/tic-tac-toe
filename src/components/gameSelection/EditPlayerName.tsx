8d010df7077d3f5219f83c899c76bf9a27c6fa73
KonstantinosKoulaxizis <kkoulaxizis07@gmail.com>
2022-09-27 17:43:38 +0200

    fix: At EditPlayerNane, fix label and add comments

--- src/components/gameSelection/EditPlayerName.tsx
+++ src/components/gameSelection/EditPlayerName.tsx
@@ -10,6 +10,11 @@ const EditPlayerName = () => {
   const dispatch = useReduxDispatch()
   const { playerNames } = useReduxSelector(state => state.game)
 
+  /**
+   *
+   * @param event String indicating players name
+   * @param mark Player's mark used as key at playerNames
+   */
   const handleUpdate = (event: ChangeEvent<HTMLInputElement>, mark: MarkType) => {
     const value = event.target.value || ''
     dispatch(setPlayerName({ mark, value }))
@@ -20,13 +25,13 @@ const EditPlayerName = () => {
       <h2 className='reminder'>Remember X goes first</h2>
       {/* Loop through marks array and use it as key to get the name value*/}
       {MARKS.map(mark => (
-        <label key={mark}>
-          <span className='label-tag'>
+        <div key={mark}>
+          <label htmlFor={mark} className='label-tag'>
             <span>{mark} player's name</span>
-          </span>
+          </label>
 
-          <input value={playerNames[mark]} onChange={e => handleUpdate(e, mark)} />
-        </label>
+          <input id={mark} value={playerNames[mark]} onChange={e => handleUpdate(e, mark)} />
+        </div>
       ))}
     </div>
   )
