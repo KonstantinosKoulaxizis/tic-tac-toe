3e7aae6615c5b19f51dd0c912d5dfab7fab08249
KonstantinosKoulaxizis <kkoulaxizis07@gmail.com>
2022-09-29 09:19:15 +0200

    feat: Add on air logo

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -4,6 +4,7 @@ import Mark from './Mark'
 const Logo = () => {
   return (
     <div className='logo'>
+      <span className='on-air-logo'>ON AIR</span>
       {MARKS.map(mark => (
         <span key={mark} className='logo-letter'>
           <Mark mark={mark} />
