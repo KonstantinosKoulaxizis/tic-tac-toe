dd5335080db4c1c9c893bbf164a6fa9ede0cd90b
Konstantinos K <47348484+KonstantinosKoulaxizis@users.noreply.github.com>
2022-12-21 10:32:06 +0100

    Update App.tsx
    Declare global window

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import { Routes, Route, Outlet } from 'react-router-dom'
 import GameSelection from './views/GameSelection'
 import Game from './views/Game'
 
+declare global {
+    interface Window {
+        analytics?: {
+            identify?: IdentifyActionType;
+        };
+    }
+}
+
 export default function App() {
   // test segment
   window.analytics.identify('test_user',{
